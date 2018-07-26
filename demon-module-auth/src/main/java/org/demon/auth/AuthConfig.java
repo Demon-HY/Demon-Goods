0aa4f96869588918d69a8ec2f55729c2e7821c2d
heyan <heyan@xubei>
2018-07-26 12:16:44 +0800

    删除无用的工程

--- demon-module-auth/src/main/java/org/demon/auth/AuthConfig.java
+++
@@ -1,8 +0,0 @@
-package org.demon.auth;
-
-import org.demon.starter.module.DefaultConfig;
-
-public class AuthConfig extends DefaultConfig {
-
-    public static final String MODULE_NAME = "auth";
-}
--- demon-module-right/src/main/java/org/demon/right/RightConfig.java
+++
@@ -1,8 +0,0 @@
-package org.demon.right;
-
-import org.demon.starter.module.DefaultConfig;
-
-public class RightConfig extends DefaultConfig {
-
-    public static final String MODULE_NAME = "role";
-}
